4f05c8052a969ad88b6f903749a94c0c252243ca
Ramone Robertson <ramonerobertson10@gmail.com>
2025-02-26 17:03:09 +0900

    Spring new

---
+++ src/main/java/com/example/spring_run_tracker_api/SpringRunTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_run_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRunTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRunTrackerApiApplication.class, args);
+	}
+
+}
