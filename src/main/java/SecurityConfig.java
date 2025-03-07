8abe8465a9ab04f66d884b69fba7217281866aa4
Ramone Robertson <ramonerobertson10@gmail.com>
2025-03-07 22:36:12 +0900

    Removed web token dependency

--- src/main/java/SecurityConfig.java
+++ src/main/java/SecurityConfig.java
@@ -1,2 +1,10 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
 public class SecurityConfig {
 }
